f1ff25f03a8108749ae38e80412e146087fd38a2
Mauricio <mauriciomatos44@gmail.com>
2023-10-11 13:06:12 -0700

    probando el git ignore

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 5555;
 
-export const MONGODBURL = "mongodb+srv://Punisher:a159875321@cluster0.y0mm9nj.mongodb.net/books-collection?retryWrites=true&w=majority";
+export const MONGODBURL = "";
 
 //probando
